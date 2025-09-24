c0a65f97ad3af64862a900a1b6eaa2a4f2eae088
rashwanth17 <rashwanth90474@gmail.com>
2025-09-24 19:46:29 +0530

    Wrote code to get input using fgets()

--- Calculator_assignment_1/calculator.c
+++ Calculator_assignment_1/calculator.c
@@ -1,1 +1,10 @@
-// starting the calculator project
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char exp[100];
+    printf("Enter the Mathematical Expression: ");
+    fgets(exp,sizeof(exp),stdin);
+    printf("%s",exp);
+    return 0;
+}
