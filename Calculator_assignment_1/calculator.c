41e494540d5c5e3c14b049d7979077ecd5fe4e66
rashwanth17 <rashwanth90474@gmail.com>
2025-09-25 09:57:36 +0530

    handling white spaces in the expression

--- Calculator_assignment_1/calculator.c
+++ Calculator_assignment_1/calculator.c
@@ -5,6 +5,10 @@ int main()
     char exp[100];
     printf("Enter the Mathematical Expression: ");
     fgets(exp,sizeof(exp),stdin);
-    printf("%s",exp);
+    for(int i=0;exp[i]!='\0';i++)
+    {
+        if(isspace(exp[i])) continue;
+        printf("%c",exp[i]);
+    }
     return 0;
 }
