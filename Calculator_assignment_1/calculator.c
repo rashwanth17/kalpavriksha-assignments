7744d9ea0cb46104d3425705702a597f6f277a47
rashwanth17 <rashwanth90474@gmail.com>
2025-09-25 20:23:49 +0530

    used stack to store numbers from the experssion

--- Calculator_assignment_1/calculator.c
+++ Calculator_assignment_1/calculator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include<ctype.h>
+#define MAX 1000
+int stack[MAX];
+int top=-1;
 int main()
 {
     char exp[100];
@@ -8,7 +11,26 @@ int main()
     for(int i=0;exp[i]!='\0';i++)
     {
         if(isspace(exp[i])) continue;
-        printf("%c",exp[i]);
+        int num=0;
+        int j=i;
+        while(isdigit(exp[j]))
+        {
+            num=num*10+(exp[j]-'0');
+            j++;
+        }
+        if(j!=i) i=j-1;
+
+        if(num!=0 && top!=MAX-1)
+        {
+            top++;
+            stack[top]=num;
+        }
+        // printf("%c",exp[i]);
+    }
+
+    for(int i=0;i<=top;i++)
+    {
+        printf("%d ",stack[i]);
     }
     return 0;
 }
