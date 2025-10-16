693b814d6c602b11463014ff2f3ea173c9f50a57
rashwanth17 <rashwanth90474@gmail.com>
2025-10-16 21:27:32 +0530

    Refactor matrix operations and add smoothing function and used malloc instead of 2d array

--- assignment-4/matrix.c
+++ assignment-4/matrix.c
@@ -2,67 +2,114 @@
 #include<time.h>
 #include<stdlib.h>
 
-void display_matrix(int matrix_size,int *p)
+void display_matrix(int matrix_size,int *matrix_ptr)
 {
     for(int row=0;row<matrix_size;row++)
     {
         for(int column=0;column<matrix_size;column++)
         {
-            printf("%d\t",*(p+row*matrix_size+column));
+            printf("%d\t",*(matrix_ptr+row*matrix_size+column));
         }
         printf("\n");
     }
 }
 
-void generate_random_matrix(int matrix_size,int *p)
+void generate_random_matrix(int matrix_size,int *matrix_ptr)
 {
     srand(time(0));
     for(int row=0;row<matrix_size;row++)
     {
         for(int column=0;column<matrix_size;column++)
         {
-            *(p+row*matrix_size+column)=(rand()%255)+1;
+            *(matrix_ptr+row*matrix_size+column)=(rand()%255)+1;
         }
     }
 }
 
-void rotate_matrix(int matrix_size,int *p)
+void smooth_matrix(int matrix_size,int *matrix_ptr)
+{
+    for(int row=0;row<matrix_size;row++)
+    {
+        for(int column=0;column<matrix_size;column++)
+        {
+            int sum=0,count=0;
+            for(int i=row-1;i<=row+1;i++)
+            {
+                for(int j=column-1;j<=column+1;j++)
+                {
+                    if(i>=0 && i<matrix_size && j>=0 && j<matrix_size)
+                    {
+                        sum+=(*(matrix_ptr+i*matrix_size+j));
+                        count++;
+                    }
+                }
+            }
+            
+            int average=sum/count;
+            *(matrix_ptr+row*matrix_size+column)=average;
+        }
+    }
+    printf("Smoothed Matrix:\n");
+    display_matrix(matrix_size,matrix_ptr);
+}
+
+void rotate_matrix(int matrix_size,int *matrix_ptr)
 {
     for(int row=0;row<matrix_size;row++)
     {
         for(int column=row;column<matrix_size;column++)
         {
-            int *a=p+row*matrix_size+column;
-            int *b=p+column*matrix_size+row;
-            int temp=*a;
-            *a=*b;
-            *b=temp;
+            int *element1=matrix_ptr+row*matrix_size+column;
+            int *element2=matrix_ptr+column*matrix_size+row;
+            int temp=*element1;
+            *element1=*element2;
+            *element2=temp;
         }
     }
+
     for(int row=0;row<matrix_size;row++)
     {
         for(int column=0;column<matrix_size/2;column++)
         {
-            int *a=p+row*matrix_size+column;
-            int *b=p+row*matrix_size+(matrix_size-1-column);
-            int temp=*a;
-            *a=*b;
-            *b=temp;
+            int *left_element=matrix_ptr+row*matrix_size+column;
+            int *right_element=matrix_ptr+row*matrix_size+(matrix_size-1-column);
+            int temp=*left_element;
+            *left_element=*right_element;
+            *right_element=temp;
         }
     }
-    display_matrix(matrix_size,p);
+    printf("Rotated Matrix:\n");
+    display_matrix(matrix_size,matrix_ptr);
 }
 
 int main()
 {
     int matrix_size;
-    printf("Enter the size of the matrix: ");
+    printf("Enter the size of the matrix (2-255): ");
     scanf("%d",&matrix_size);
-    int matrix[matrix_size][matrix_size];
-    int *p=&matrix[0][0];
-    generate_random_matrix(matrix_size,p);
+
+    if(matrix_size<2||matrix_size>255)
+    {
+        printf("Invalid input. Size must be between 2 and 255.\n");
+        return 1;
+    }
+
+    int *matrix_ptr=(int*)malloc(matrix_size*matrix_size*sizeof(int));
+
+    if(matrix_ptr==NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    generate_random_matrix(matrix_size,matrix_ptr);
+
     printf("Randomly Generated Matrix:\n");
-    display_matrix(matrix_size,p);
-    printf("Rotated matrix by 90 degrees:\n");
-    rotate_matrix(matrix_size,p);
+    display_matrix(matrix_size,matrix_ptr);
+
+    rotate_matrix(matrix_size,matrix_ptr);
+    smooth_matrix(matrix_size,matrix_ptr);
+
+    free(matrix_ptr);
+
+    return 0;
 }
