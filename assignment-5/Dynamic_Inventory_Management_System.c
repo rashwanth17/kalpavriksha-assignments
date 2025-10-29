4a5ad791fce42467261b5cd69b4d42dd119ffb66
rashwanth17 <rashwanth90474@gmail.com>
2025-10-29 10:01:09 +0530

    Improve input validation and user prompts

--- assignment-5/Dynamic_Inventory_Management_System.c
+++ assignment-5/Dynamic_Inventory_Management_System.c
@@ -40,121 +40,149 @@ int isDuplicateId(int product_ID,Product *inventory,int number_of_products)
 int getValidProductID()
 {
     char product_ID_text[5];
-    scanf("%s",product_ID_text);
+    int product_ID;
 
-    while(getchar()!='\n');
-
-    if(!validInput(product_ID_text))
+    while(1)
     {
-        printf("Error: Enter proper Product Id!\n");
-        exit(0);
-    }
+        printf("Product ID: ");
+        scanf("%s",product_ID_text);
 
-    int product_ID=atoi(product_ID_text);
+        while(getchar()!='\n');
 
-    if(product_ID<1 || product_ID>10000)
-    {
-        printf("Error: Product ID must be in the range between 1 to 10000!\n");
-        exit(0);
+        if(!validInput(product_ID_text))
+        {
+            printf("Error: Enter proper Product Id!\n");
+            continue;
+        }
+
+        product_ID=atoi(product_ID_text);
+
+        if(product_ID<1 || product_ID>10000)
+        {
+            printf("Error: Product ID must be in the range between 1 to 10000!\n");
+            continue;
+        }
+        return product_ID;
     }
-    return product_ID;
 }
 
 void getValidProductName(int index,Product *inventory)
 {
     char product_name[60];
-    fgets(product_name,sizeof(product_name),stdin);
 
-    int length=strlen(product_name);
-
-    if(length>0 && product_name[length-1]=='\n')
+    while(1)
     {
-        product_name[length-1]='\0';
-        length--;
-    }
+        printf("Product Name: ");
+        fgets(product_name,sizeof(product_name),stdin);
 
-    if(length>50)
-    {
-        printf("Error: The length of the Product Name should not exceed 50 characters!\n");
-        exit(0);
-    }
+        int length=strlen(product_name);
 
-    if(length==0)
-    {
-        printf("Error: Product Name should not be empty!\n");
-        exit(0);
-    }
+        if(length>0 && product_name[length-1]=='\n')
+        {
+            product_name[length-1]='\0';
+            length--;
+        }
+
+        if(length>50)
+        {
+            printf("Error: The length of the Product Name should not exceed 50 characters!\n");
+            continue;
+        }
+
+        if(length==0)
+        {
+            printf("Error: Product Name should not be empty!\n");
+            continue;
+        }
     
-    strcpy(inventory[index].product_name,product_name);
+        strcpy(inventory[index].product_name,product_name);
+        break;
+    }
 }
 
 float getValidProductPrice()
 {
     char product_price[20];
-    fgets(product_price,sizeof(product_price),stdin);
+    float price_value;
 
-    int length=strlen(product_price);
-
-    if(length>0 && product_price[length-1]=='\n')
+    while(1)
     {
-        product_price[length-1]='\0';
-        length--;
-    }
+        printf("Product Price: ");
+        fgets(product_price,sizeof(product_price),stdin);
 
-    int dot_count=0;
+        int length=strlen(product_price);
 
-    for(int i=0;*(product_price+i)!='\0';i++)
-    {
-        if(*(product_price+i)=='.')
+        if(length>0 && product_price[length-1]=='\n')
         {
-            dot_count++;
-        }
-        else if(!isdigit(*(product_price+i)))
-        {
-            printf("Error: Enter a valid input for price!\n");
-            exit(0);
+            product_price[length-1]='\0';
+            length--;
         }
 
-        if(dot_count>1)
+        int dot_count=0,valid=1;
+
+        for(int i=0;*(product_price+i)!='\0';i++)
         {
-            printf("Error: Enter a valid input for price!\n");
-            exit(0);
+            if(*(product_price+i)=='.')
+            {
+                dot_count++;
+            }
+            else if(!isdigit(*(product_price+i)))
+            {
+                valid=0;
+            }
+
+            if(dot_count>1)
+            {
+                valid=0;
+            }
         }
+
+        if(!valid)
+        {
+            printf("Error: Enter a valid numeric price!\n");
+            continue;
+        }
+
+        price_value=atof(product_price);
+
+        if(price_value<0 || price_value>100000)
+        {
+            printf("Error: price value should be in the range 0 to 100000!\n");
+            continue;
+        }
+
+        return price_value;
     }
-
-    float price_value=atof(product_price);
-
-    if(price_value<=0 || price_value>100000)
-    {
-        printf("Error: price value should be in the range 1 to 10000!\n");
-        exit(0);
-    }
-
-    return price_value;
 }
 void getValidProductQuantity(int index,Product *inventory)
 {
     char product_quantity_text[8];
-    scanf("%s",product_quantity_text);
+    int product_quantity;
 
-    while(getchar()!='\n');
-
-    if(!validInput(product_quantity_text))
+    while(1)
     {
-        printf("Error: Enter proper Product quantity!\n");
-        return;
+        printf("Product Quantity: ");
+        scanf("%s",product_quantity_text);
+
+        while(getchar()!='\n');
+
+        if(!validInput(product_quantity_text))
+        {
+            printf("Error: Enter proper Product quantity!\n");
+            continue;
+        }
+
+        product_quantity=atoi(product_quantity_text);
+
+        if(product_quantity<1 || product_quantity>100000)
+        {
+            printf("Error: Product quantity must be in the range between 1 to 100000!\n");
+            continue;
+        }
+
+        inventory[index].product_quantity=product_quantity;
+        break;
     }
-
-    int product_quantity=atoi(product_quantity_text);
-
-    if(product_quantity<1 || product_quantity>100000)
-    {
-        printf("Error: Product quantity must be in the range between 1 to 100000!\n");
-        return;
-    }
-
-    inventory[index].product_quantity=product_quantity;
-    printf("Quantity updated successfully!\n");
 }
 
 void getProductDetails(Product *inventory,int number_of_products)
@@ -163,7 +191,6 @@ void getProductDetails(Product *inventory,int number_of_products)
     {
         printf("Enter details for product %d\n",index+1);
 
-        printf("Product ID: ");
         int product_ID=getValidProductID();
 
         if(isDuplicateId(product_ID,inventory,index))
@@ -174,14 +201,11 @@ void getProductDetails(Product *inventory,int number_of_products)
         }
         inventory[index].product_ID=product_ID;
 
-        printf("Product Name: ");
         getValidProductName(index,inventory);
 
-        printf("Product Price: ");
         float product_price=getValidProductPrice();
         inventory[index].product_price=product_price;
 
-        printf("Product Quantity: ");
         getValidProductQuantity(index,inventory);
         printf("\n");
     }
@@ -203,7 +227,6 @@ void addNewProduct(int *number_of_products,Product **inventory)
 
     printf("Enter new product details:\n");
 
-    printf("Product ID: ");
     int product_ID=getValidProductID();
     if(isDuplicateId(product_ID,*inventory,*number_of_products-1))
     {
@@ -213,14 +236,11 @@ void addNewProduct(int *number_of_products,Product **inventory)
     }
     (*inventory)[index].product_ID=product_ID;
 
-    printf("Product Name: ");
     getValidProductName(index,*inventory);
 
-    printf("Product Price: ");
     float product_price=getValidProductPrice();
     (*inventory)[index].product_price=product_price;
 
-    printf("Product Quantity: ");
     getValidProductQuantity(index,*inventory);
 
     printf("Product added successfully!\n");
@@ -277,7 +297,6 @@ void updateQuantity(Product *inventory,int number_of_products)
 
 void searchProductByID(Product *inventory,int number_of_products)
 {
-    printf("Enter Product ID to search: ");
     int product_ID=getValidProductID();
     int index=findProductByID(product_ID,inventory,number_of_products);
 
@@ -373,7 +392,6 @@ void searchProductByPriceRange(Product *inventory,int number_of_products)
 
 void deleteProduct(Product **inventory,int *number_of_products)
 {
-    printf("Enter Product ID to delete the product: ");
     int product_ID=getValidProductID();
     int index=findProductByID(product_ID,*inventory,*number_of_products);
 
@@ -389,6 +407,14 @@ void deleteProduct(Product **inventory,int *number_of_products)
     }
 
     (*number_of_products)--;
+
+    if(*number_of_products==0)
+    {
+        free(*inventory);
+        *inventory=NULL;
+        printf("All products deleted.\n");
+        return;
+    }
 
     Product *temporary_pointer=(Product *)realloc(*inventory,(*number_of_products)*sizeof(Product));
 
@@ -409,23 +435,27 @@ int main()
     char number_of_products_text[5];
     int number_of_products;
 
-    printf("Enter initial number of products (1-100): ");
-    scanf("%s",number_of_products_text);
-
-    while(getchar()!='\n');
-
-    if(!validInput(number_of_products_text))
+    while(1)
     {
-        printf("Error: Enter proper Integer value between 1 to 100.\n");
-        return 0;        
-    }
+        printf("Enter initial number of products (1-100): ");
+        scanf("%s",number_of_products_text);
 
-    number_of_products=atoi(number_of_products_text);
+        while(getchar()!='\n');
 
-    if(number_of_products<1 || number_of_products>100)
-    {
-        printf("Error: Enter proper Integer value between 1 to 100.\n");
-        return 0;
+        if(!validInput(number_of_products_text))
+        {
+            printf("Error: Enter proper Integer value between 1 to 100.\n");
+            continue;        
+        }
+
+        number_of_products=atoi(number_of_products_text);
+
+        if(number_of_products<1 || number_of_products>100)
+        {
+            printf("Error: Enter proper Integer value between 1 to 100.\n");
+            continue;
+        }
+        break;
     }
 
     Product *inventory=(Product *)calloc(number_of_products,sizeof(Product));
@@ -452,57 +482,65 @@ int main()
         printf("6. Search Product by Price Range\n");
         printf("7. Delete Product\n");
         printf("8. Exit\n");
-        printf("Enter your choice: \n");
-        scanf("%s",choice_text);
+        
 
-        while(getchar()!='\n');
-
-        if(!validInput(choice_text))
+        while(1)
         {
-            printf("Error: Enter a valid number between 1 to 8!\n");
-            exit(0);
-        }
+            printf("Enter your choice: \n");
+            scanf("%s",choice_text);
 
-        choice=atoi(choice_text);
+            while(getchar()!='\n');
+
+            if(!validInput(choice_text))
+            {
+                printf("Error: Enter a valid number between 1 to 8!\n");
+                continue;
+            }
+
+            choice=atoi(choice_text);
+            if(choice<1 || choice>8)
+            {
+                printf("Error: Enter a valid number between 1 to 8\n");
+                continue;
+            }
+            break;
+        }
      
         switch (choice)
         {
-        case 1:
-            addNewProduct(&number_of_products,&inventory);
-            break;
+            case 1:
+                addNewProduct(&number_of_products,&inventory);
+                break;
 
-        case 2:
-            viewProducts(number_of_products,inventory);
-            break;
+            case 2:
+                viewProducts(number_of_products,inventory);
+                break;
 
-        case 3:
-            updateQuantity(inventory,number_of_products);
-            break;
+            case 3:
+                updateQuantity(inventory,number_of_products);
+                break;
 
-        case 4:
-            searchProductByID(inventory,number_of_products);
-            break;
+            case 4:
+                searchProductByID(inventory,number_of_products);
+                break;
 
-        case 5:
-            searchProductByName(inventory,number_of_products);
-            break;
+            case 5:
+                searchProductByName(inventory,number_of_products);
+                break;
 
-        case 6:
-            searchProductByPriceRange(inventory,number_of_products);
-            break;
+            case 6:
+                searchProductByPriceRange(inventory,number_of_products);
+                break;
 
-        case 7:
-            deleteProduct(&inventory,&number_of_products);
-            break;
+            case 7:
+                deleteProduct(&inventory,&number_of_products);
+                break;
 
-        case 8:
-            printf("Memory released successfully. Exiting program...!\n");
-            free(inventory);
-            return 1;
-
-        default:
-            printf("Error: Enter a valid number between 1 to 8!\n");
-            break;
+            case 8:
+                printf("Memory released successfully. Exiting program...!\n");
+                free(inventory);
+                inventory=NULL;
+                return 0;
         }
 
     }
